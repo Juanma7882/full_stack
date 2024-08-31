365e10b77fe9a21e0087be6adfc64646b1c5984e
juan <juanmaf236@gmail.com>
2024-08-31 18:56:11 -0300

    week 8 proyecto terminado

--- week_8/day_44/clase_017_NucbaNFT/assets/index.js
+++ week_8/day_44/clase_017_NucbaNFT/assets/index.js
@@ -16,7 +16,12 @@ const buyBtn = document.querySelector(".btn-buy");
 const deleteBtn = document.querySelector(".btn-delete");
 const productsCart = document.querySelector(".cart-container");
 
-let cart = [];
+const succesModal = document.querySelector(".add-modal");
+let cart = JSON.parse(localStorage.getItem(`cart`)) || [];
+
+const saveLocalStorage = () => {
+  localStorage.setItem(`cart`, JSON.stringify(cart));
+};
 
 const createProductTemplate = (product) => {
   return `<div class="product">
@@ -169,35 +174,92 @@ const closeOnScroll = () => {
   overlay.classList.remove("show-overlay");
 };
 
+const createCartProductTemplate = (cartproduct) => {
+  const { bid, id, img, name, quantity } = cartproduct;
+
+  return `
+  <div class="cart-item">
+            <img src="${img}" alt="${name}" />
+            <div class="item-info">
+              <h3 class="item-title">${name}</h3>
+              <p class="item-bid">Current bid</p>
+              <span class="item-price">${bid} ETH</span>
+            </div>
+            <div class="item-handler">
+              <span class="quantity-handler down" data-id=${id}>-</span>
+              <span class="item-quantity">${quantity}</span>
+              <span class="quantity-handler up" data-id=${id}>+</span>
+            </div>
+          </div>
+  `;
+};
+
 const renderCart = () => {
   if (!cart.length) {
     productsCart.innerHTML = `<p class="empty-msg"> Agrega un Producto </p>`;
   } else {
     console.log("thanks");
-    productsCart.innerHTML = `<p class="empty-msg"> Gracias por comprar </p>`;
+    // productsCart.innerHTML = `<p class="empty-msg"> Gracias por comprar </p>`;
+    productsCart.innerHTML = cart.map(createCartProductTemplate).join("");
   }
+};
+
+const getCartTotal = () => {
+  return cart.reduce((acc, cur) => acc + Number(cur.bid) * cur.quantity, 0);
+};
+
+const showCartTotal = () => {
+  total.innerHTML = `${getCartTotal().toFixed(2)}`;
+  console.log(getCartTotal());
+};
+
+//? actualizar la burbuja
+
+const renderCartBuble = () => {
+  cartBubble.textContent = cart.reduce((acc, cur) => acc + cur.quantity, 0);
+};
+
+const disableBtn = (btn) => {
+  if (!cart.length) {
+    btn.classList.add(`disabled`);
+  } else {
+    btn.classList.remove(`disabled`);
+  }
+};
+
+const updateCartState = () => {
+  saveLocalStorage();
+  renderCart();
+  showCartTotal();
+  disableBtn(buyBtn);
+  disableBtn(deleteBtn);
+  renderCartBuble();
+  console.log(` render cart bublu ${renderCartBuble()}`);
 };
 
 const addProduct = (e) => {
-  console.log(e.target);
+  // console.log(e.target);
   if (!e.target.classList.contains("btn-add")) return;
   const product = e.target.dataset;
+
   console.log(product);
   if (IsExistCartProduct(product)) {
     addUnitToProduct(product);
+    showSuccesModal("producto añadido");
   } else {
     createCartProduct(product);
+    showSuccesModal("producto añadido");
   }
-  console.log(cart);
-  renderCart();
+  updateCartState();
 };
 
 const addUnitToProduct = (product) => {
-  cart = cart.map((cartproduct) =>
-    cartproduct.id === cartproduct.id
-      ? { ...cartproduct, quantity: cartproduct.quantity + 1 }
-      : cartproduct
+  cart = cart.map((cartProduct) =>
+    cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct
   );
+  console.log(cart);
 };
 
 const IsExistCartProduct = (product) => {
@@ -206,6 +268,74 @@ const IsExistCartProduct = (product) => {
 
 const createCartProduct = (product) => {
   cart = [...cart, { ...product, quantity: 1 }];
+};
+
+const showSuccesModal = (msg) => {
+  succesModal.classList.add(`active-modal`);
+  succesModal.textContent = msg;
+  setTimeout(() => {
+    succesModal.classList.remove(`active-modal`);
+  }, 1500);
+};
+
+const handlePlusBtnEvent = (id) => {
+  const existingCartProduct = cart.find((item) => item.id === id);
+  addUnitToProduct(existingCartProduct);
+};
+
+const substractProductUnit = (existingCartProduct) => {
+  cart = cart.map((product) => {
+    return product.id === existingCartProduct.id
+      ? { ...product, quantity: Number(product.quantity) - 1 }
+      : product;
+  });
+};
+
+const handleMinusBtnEvent = (id) => {
+  const existingCartProduct = cart.find((item) => item.id === id);
+  substractProductUnit(existingCartProduct);
+  if (existingCartProduct.quantity === 1) {
+    if (window.confirm("deseas eleiminar el producto")) {
+      removeProductFromCart(existingCartProduct);
+    }
+    return;
+  }
+};
+
+const removeProductFromCart = (existingCartProduct) => {
+  cart = cart.filter((product) => product.id != existingCartProduct.id);
+  updateCartState();
+};
+const handleQuantity = (e) => {
+  if (e.target.classList.contains(`up`)) {
+    handlePlusBtnEvent(e.target.dataset.id);
+    console.log("estoy en handlequantity up");
+  } else if (e.target.classList.contains(`down`)) {
+    console.log("estoy en handlequantity down");
+    handleMinusBtnEvent(e.target.dataset.id);
+  }
+
+  updateCartState();
+};
+
+const resetCartItem = () => {
+  cart = [];
+  updateCartState();
+};
+
+const completeCartAction = (confirmMsg, succesMsg) => {
+  if (!cart.length) return;
+  if (window.confirm(confirmMsg)) {
+    resetCartItem();
+    alert(succesMsg);
+  }
+};
+
+const completeBuy = () => {
+  completeCartAction("Deseas completar tu compra?", "Gracias por tu compra");
+};
+const deleteCart = () => {
+  completeCartAction("Deseas borrar el carro?", "No hay productos en el carro");
 };
 
 const init = () => {
@@ -220,7 +350,18 @@ const init = () => {
   window.addEventListener("scroll", closeOnScroll);
 
   productsContainer.addEventListener("click", addProduct);
+  productsCart.addEventListener("click", handleQuantity);
   document.addEventListener("DOMContentLoaded", renderCart);
+
+  buyBtn.addEventListener("click", completeBuy);
+  deleteBtn.addEventListener("click", deleteCart);
+
+  //! se puede hacer de cualquiera de las 2 formas
+  cartBubble.addEventListener("DOMContentLoaded", renderCartBuble(cart));
+  // renderCartBuble(cart)
+
+  disableBtn(buyBtn);
+  disableBtn(deleteBtn);
 };
 
 init();
